288ab95bdafa22912f2c9fe0c37e22ea32ea24eb
rcamarano <rogeriocamarano@gmail.com>
2023-07-21 18:40:41 -0300

    req 3, Maping orders and ingredients, passing on local tests

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,29 @@
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.load_menu_data(source_path)
+
+    def load_menu_data(self, source_path: str) -> None:
+        with open(source_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            dishes = {}
+            for row in reader:
+                dish_name = row['dish']
+                dish_price = float(row['price'])
+                ingredient_name = row['ingredient']
+                recipe_amount = int(row['recipe_amount'])
+
+                dish = dishes.get(dish_name)
+                if dish is None:
+                    dish = Dish(dish_name, dish_price)
+                    dishes[dish_name] = dish
+                    self.dishes.add(dish)
+
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(ingredient, recipe_amount)
