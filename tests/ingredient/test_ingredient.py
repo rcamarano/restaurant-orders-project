64e45775b2ab6ff193d8b0b7a05b7e721439335a
rcamarano <rogeriocamarano@gmail.com>
2023-07-21 14:40:22 -0300

    req 1, Testing implemented classes part 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+) # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    frango = Ingredient('frango')
+    ovo = Ingredient('ovo')
+    farinha = Ingredient('farinha')
+
+    assert farinha != ovo
+    assert farinha == farinha
+
+    assert ovo.__hash__() != farinha.__hash__()
+    assert ovo.__hash__() != frango.__hash__()
+    assert ovo.__hash__() == ovo.__hash__()
+
+    assert repr(ovo) == "Ingredient('ovo')"
+    assert ovo.name == 'ovo'
+
+    assert farinha.restrictions == {Restriction.GLUTEN}
